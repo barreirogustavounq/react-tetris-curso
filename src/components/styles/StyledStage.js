096b1677ac7e0e97f44cf4e88e7c20da6fe38b3a
Gustavo Barreiro <37371142+barreirogustavounq@users.noreply.github.com>
2021-11-25 22:38:01 -0300

    cambios de estilos

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -2,14 +2,13 @@ import styled from "styled-components";
 /** Estilos de la pantalla */
 export const StyledStage = styled.div`
   display: grid;
-  grid-template-rows: repeat(
-    ${(props) => props.height},
-    calc(25vw / ${(props) => props.width})
-  );
-  grid-template-columns: repeat(${(props) => props.width}, 1fr);
-  grid-gap: 1px;
-  border: 2px solid #333;
+  grid-template-columns: repeat(12, 1fr);
+  gap: 1px;
+  border: 2px solid rgb(51, 51, 51);
   width: 100%;
   max-width: 25vw;
-  background: #111;
+  background: rgb(17, 17, 17);
+  POSITION: absolute;
+  min-width: 18rem;
+  min-height: 32rem;
 `;
